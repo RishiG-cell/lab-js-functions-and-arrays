20905eff38a501ea013d80079a6ffd5a2fd27a1c
Rishi G. <mail.rishig@gmail.com>
2025-05-08 15:01:58 +0200

    lab done

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,5 +1,11 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(num1, num2) {
+    if(num1 > num2){
+        return num1;
+    } else {
+        return num2;
+    }
+}
 
 
 
@@ -7,7 +13,19 @@ function maxOfTwoNumbers() {}
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
+function findLongestWord(array) {
+ let longestWord= '';
+ if (array.length === 0){
+    return null;
+ }
+array.forEach((array) => { 
+    if (array.length > longestWord.length){
+        longestWord = array;
+    } 
+});
+return longestWord;
+
+}
 
 
 
@@ -15,7 +33,13 @@ function findLongestWord() {}
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(arrayNum) {
+    let sumTotal = 0;
+    for(i = 0; i < arrayNum.length; i++){
+        sumTotal = sumTotal + arrayNum[i]
+    }
+    return sumTotal ;
+}
 
 
 
@@ -23,7 +47,14 @@ function sumNumbers() {}
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(numbersArray) {
+    if(numbersArray.length === 0){
+        return 0;
+    } 
+    let total = sumNumbers(numbersArray);
+    return total / numbersArray.length;
+    
+}
 
 
 
@@ -31,4 +62,16 @@ function averageNumbers() {}
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
 
-function doesWordExist() {}
+function doesWordExist(words2Array, inArray) {
+
+    if(words2Array.length === 0){
+        return null;
+    }
+    let isThere = false;
+    words2Array.forEach((thingInside) => {
+        if (thingInside === inArray){
+            isThere = true;
+        }
+    })
+    return isThere
+}
